5d0c543f5d04c439bcabff21873b6e7aab793bef
Bangash38 <bangashmhasnain38@gmail.com>
2024-11-18 20:08:05 +0500

    Initial Commit

---
+++ FacebookClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FacebookClone
+//
+//  Created by Muhammad Hasnain Bangash on 18/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FacebookCloneTests/FacebookCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookCloneTests.swift
+//  FacebookCloneTests
+//
+//  Created by Muhammad Hasnain Bangash on 18/11/2024.
+//
+
+import Testing
+@testable import FacebookClone
+
+struct FacebookCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
